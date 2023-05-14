f17fcbaa105166d40dab35a3f3c66ea60d7994b9
arty4325 <arturo.acuna.duran@gmail.com>
2023-05-14 12:10:03 -0600

    Merge de main en Arturo

---
+++ Proyecto/SocketThread2.cpp
@@ -0,0 +1,5 @@
+//
+// Created by oscaraad on 14/05/23.
+//
+
+#include "SocketThread2.h"
---
+++ Proyecto/SocketThread2.h
@@ -0,0 +1,14 @@
+//
+// Created by oscaraad on 14/05/23.
+//
+
+#ifndef PACMAN_SOCKETTHREAD2_H
+#define PACMAN_SOCKETTHREAD2_H
+
+
+class SocketThread2 {
+
+};
+
+
+#endif //PACMAN_SOCKETTHREAD2_H
